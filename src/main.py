0e267dccacf30c9dac98e62ffe44e8412d791709
ellesimas <ellesimas@gmail.com>
2018-02-08 15:24:44 -0200

    Classe tesouro

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 class Jogo:
+
     def __init__(self):
         pass
 
     def inicia(self):
         self
 
+class Tesouro:
+
+    def __init__(self):
+        pass
+
+    def apresent(self):
+        pass
